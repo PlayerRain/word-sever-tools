58d9e476a46dffb767d3e23068c6aec9b1ee310e
PlayerRain <ch13829395504@outlook.com>
2025-08-07 22:08:35 +0800

    Delete src/config.ts

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const ip = "localhost";
-export const port = 5231;
-export const ws_port = 5232;
-export const server_address = `http://${ip}:${port}`;//server_address
-export const ban_cheat = false;//反作弊
